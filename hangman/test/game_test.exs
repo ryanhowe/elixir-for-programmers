2e653e68ba0bc18aec344a2acfa7591363a35274
Ryan Howe <howe.ryan@gmail.com>
2017-12-10 16:43:58 -0500

    -- Game and api implemented

--- hangman/test/game_test.exs
+++ hangman/test/game_test.exs
@@ -14,20 +14,78 @@ defmodule GameTest do
         for state <- [:won, :lost] do
             game = Game.new_game()
                 |> Map.put(:game_state, state)
-            assert {^game, _} = Game.make_move(game, "a")
+            assert ^game = Game.make_move(game, "a")
         end
     end
 
     test "first occurrance of letter is not already used" do
         game = Game.new_game()
-        {game, _tally} = Game.make_move(game, "x")
+        game = Game.make_move(game, "x")
         assert game.game_state != :already_used
     end
 
     test "second occurrance of letter is already used" do
         game = Game.new_game()
-        {game, _tally} = Game.make_move(game, "x")
-        {game, _tally} = Game.make_move(game, "x")
+        game = Game.make_move(game, "x")
+        game = Game.make_move(game, "x")
         assert game.game_state == :already_used
+    end
+
+    test "a good guess is recognized" do
+        game = Game.new_game("wibble")
+        game = Game.make_move(game,"w")
+        assert game.game_state == :good_guess
+        assert game.turns_left == 7
+    end
+
+    test "game won is recognized" do
+        game = Game.new_game("wibble")
+        game = Game.make_move(game,"w")
+        assert game.game_state == :good_guess
+        assert game.turns_left == 7
+        game = Game.make_move(game,"i")
+        assert game.game_state == :good_guess
+        assert game.turns_left == 7
+        game = Game.make_move(game,"b")
+        assert game.game_state == :good_guess
+        assert game.turns_left == 7
+        game = Game.make_move(game,"l")
+        assert game.game_state == :good_guess
+        assert game.turns_left == 7
+        game = Game.make_move(game,"e")
+        assert game.game_state == :won
+        assert game.turns_left == 7
+    end
+
+    test "bad guess is recognized" do
+        game = Game.new_game("wi")
+        game = Game.make_move(game,"z")
+        assert game.game_state == :bad_guess
+    end
+
+
+    test "game loss is recognized" do
+        game = Game.new_game("wi")
+        game = Game.make_move(game,"z")
+        assert game.game_state == :bad_guess
+        assert game.turns_left == 6
+        game = Game.make_move(game,"a")
+        assert game.game_state == :bad_guess
+        assert game.turns_left == 5
+        game = Game.make_move(game,"b")
+        assert game.game_state == :bad_guess
+        assert game.turns_left == 4
+        game = Game.make_move(game,"c")
+        assert game.game_state == :bad_guess
+        assert game.turns_left == 3
+        game = Game.make_move(game,"d")
+        assert game.game_state == :bad_guess
+        assert game.turns_left == 2
+        game = Game.make_move(game,"e")
+        assert game.game_state == :bad_guess
+        assert game.turns_left == 1
+        game = Game.make_move(game,"f")
+        assert game.game_state == :lost
+        assert game.turns_left == 1
     end
 end
