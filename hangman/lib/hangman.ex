3072150f717845bbf6188b83ebf07f9e7e540603
Ryan Howe <howe.ryan@gmail.com>
2017-12-10 10:50:35 -0500

    -- Game make move

--- hangman/lib/hangman.ex
+++ hangman/lib/hangman.ex
@@ -1,5 +1,5 @@
 defmodule Hangman do
- 
+
   alias Hangman.Game
 
   defdelegate new_game(), to:  Game
--- hangman/lib/hangman/game.ex
+++ hangman/lib/hangman/game.ex
@@ -3,6 +3,7 @@ defmodule Hangman.Game do
         turns_left: 7,
         game_state: :initializing,
         letters: [],
+        used: MapSet.new()
 
     ) 
 
@@ -11,4 +12,25 @@ defmodule Hangman.Game do
         letters: Dictionary.random_word |> String.codepoints
      }
    end 
+
+   def make_move(game= %{game_state: state}, _guess)  when state in [:won, :lost] do
+        { game, tally(game)}
+   end
+
+   def make_move(game, guess) do
+        game = accept_move(game, guess, MapSet.member?(game.used, guess))
+        {game, tally(game)}
+   end
+
+   def accept_move(game, guess, _already_guessed = true) do
+        game = Map.put(game, :game_state, :already_used)
+   end
+
+   def accept_move(game, guess, _already_guessed) do
+        Map.put(game, :used, MapSet.put(game.used,guess))
+   end
+
+   def tally(game) do
+        123
+   end
 end
