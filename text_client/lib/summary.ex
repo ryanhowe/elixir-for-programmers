444324f2554c1eecd682350e3280efa1faf02361
Ryan Howe <howe.ryan@gmail.com>
2017-12-11 13:04:08 -0500

    -- Update winning message to display the entire word

--- text_client/lib/summary.ex
+++ text_client/lib/summary.ex
@@ -1,20 +1,20 @@
 defmodule TextClient.Summary do
 
-    def display(game = %{ tally: tally }) do
-        IO.puts [
-            "\n",
-            "Word so far: #{Enum.join(tally.letters," ")}\n",
-            "Guesses left: #{tally.turns_left}\n",
-            "Letters used: #{Enum.join(tally.used,",")}\n",
-        ]
-        game
-    end
-    
-    defp prompt(game) do
-        game
-    end
+  def display(game = %{ tally: tally }) do
+    IO.puts [
+      "\n",
+      "Word so far: #{Enum.join(tally.letters," ")}\n",
+      "Guesses left: #{tally.turns_left}\n",
+      "Letters used: #{Enum.join(tally.used,",")}\n",
+    ]
+    game
+  end
 
-    defp make_move(game) do
-        game
-    end
+  defp prompt(game) do
+    game
+  end
+
+  defp make_move(game) do
+    game
+  end
 end
